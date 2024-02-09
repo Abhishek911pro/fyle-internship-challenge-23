1b814a32b50ceb2594ef0276b7905c7b71f66cf9
Abhishek_kumar <Abhishku611@gmail.com>
2024-02-09 12:46:45 +0530

    get user name and fetched api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,24 @@ import { ApiService } from './services/api.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
+
+  item="";
+
   constructor(
     private apiService: ApiService
   ) {}
 
   ngOnInit() {
     this.apiService.getUser('johnpapa').subscribe(console.log);
+  }
+
+  getInput(e:any){
+    this.item=e.target.value;
+    // console.log(this.item);
+  }
+
+  submit_input(){
+    this.apiService.getUser(this.item).subscribe(console.log);
+    console.log(this.item);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
